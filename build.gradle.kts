49e27167ca141cec784278ffc563097e94b48309
daniil.bystrov <daniil.bystrov@center2m.ru>
2024-09-01 22:20:42 +0300

    hw04 init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,85 @@
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     idea
     java
     id("org.springframework.boot") version "3.3.0" apply true
     id("io.spring.dependency-management") version "1.1.5"
     id("com.google.cloud.tools.jib") version "3.4.3"
+    id("org.openapi.generator") version "7.5.0"
+    id("io.freefair.lombok") version "8.6"
 }
 
 version = "0.0.1-SNAPSHOT"
+val jacksonDataVersion = "2.17.1"
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
+val generatedProjectDir = "$projectDir/build/generated"
+val controllerDir = "ru.bstrdn.controller"
+val modelDir = "ru.bstrdn.data.dto"
+val openapiJson = "$projectDir/src/main/resources/openapi.yml"
+
+tasks.named<GenerateTask>("openApiGenerate") {
+
+    generatorName.set("spring")
+    inputSpec.set(openapiJson)
+    outputDir.set(generatedProjectDir)
+    apiPackage.set(controllerDir)
+    modelPackage.set(modelDir)
+
+    configOptions.putAll(
+        mapOf(
+            Pair("dateLibrary", "java8-localdatetime"),
+            Pair("useSpringBoot3", "true"),
+            Pair("documentationProvider", "springdoc"),
+            Pair("interfaceOnly", "true"),
+            Pair("useTags", "true"),
+            Pair("hideGenerationTimestamp", "true"),
+            Pair("additionalModelTypeAnnotations",
+                "@lombok.Data @lombok.NoArgsConstructor @lombok.AllArgsConstructor @lombok.experimental.SuperBuilder"
+            )
+        )
+    )
+
+    doFirst {
+        delete(generatedProjectDir)
+    }
+}
+
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonDataVersion")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonDataVersion")
+    implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonDataVersion")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    //db
+    implementation("org.postgresql:postgresql:42.7.3")
+
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
+    //mapstruct \ lombok
+    implementation("org.mapstruct:mapstruct:1.5.5.Final")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+    implementation("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
 }
 
 java {
@@ -36,6 +102,19 @@ jib {
         auth {
             username = System.getenv("GITLAB_USERNAME")
             password = System.getenv("GITLAB_PASSWORD")
+        }
+    }
+}
+
+afterEvaluate {
+    extensions.configure<SourceSetContainer>("sourceSets") {
+        named("main") {
+            java {
+                setSrcDirs(listOf("src/main/java", "build/generated/src/main/java"))
+            }
+            resources {
+                setSrcDirs(listOf("src/main/resources", "build/generated/src/main/resources"))
+            }
         }
     }
 }
---
+++ src/main/java/ru/bstrdn/service/UserService.java
@@ -0,0 +1,43 @@
+package ru.bstrdn.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.bstrdn.data.dto.User;
+import ru.bstrdn.data.entity.UserEntity;
+import ru.bstrdn.data.mapper.UserMapper;
+import ru.bstrdn.data.repository.UserRepository;
+
+/**
+ * Сервис для работы с пользователями
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+  private final UserMapper userMapper;
+
+  @Transactional
+  public void createUser(User user) {
+    UserEntity userEntity = userMapper.toEntity(user);
+    userRepository.save(userEntity);
+  }
+
+  @Transactional
+  public void deleteUser(Long userId) {
+    userRepository.deleteById(userId);
+  }
+
+  @Transactional(readOnly = true)
+  public User findUserById(Long userId) {
+    UserEntity userEntity = userRepository.findById(userId).orElseThrow();
+    return userMapper.toDto(userEntity);
+  }
+
+  @Transactional
+  public void updateUser(Long userId, User user) {
+    UserEntity userEntity = userRepository.findById(userId).orElseThrow();
+    userMapper.update(user, userEntity);
+  }
+}
